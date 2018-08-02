69d44a5ebf919b438cea6402fec7b2e26a863c25
reg4 <reg4@yandex.ru>
2018-08-02 10:44:56 +0300

    Second commit

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\controllers;
 
 use yii\web\Controller;
@@ -10,7 +9,32 @@ class PostController extends Controller
 
  public function actionIndex(){
  $posts = Post::find()->all();
+
+ 	$post = new Post();
+	$post->category_id = 1;
+	$post->title = 'Header';
+	$post->excerpt = 'excerpt';
+	$post->keywords = 'keywords';
+	$post->description = 'description';
+	$post->created = "1998-02-05";
+	$post->text = "Text content here";
+	$post->save();
+
  return $this->render('index', compact('posts'));
+ }
+
+ public function actionFill(){
+ 
+	$post->category_id = '1';
+	$post->title = 'Header';
+	$post->excerpt = 'excerpt';
+	$post->keywords = 'keywords';
+	$post->description = 'description';
+	$post->created = "1998-02-05";
+	$post->text = "Text content here";
+	$post->save();
+
+
  }
 
 }
